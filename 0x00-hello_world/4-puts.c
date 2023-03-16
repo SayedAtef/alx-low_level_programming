878fbe461d381a904c7ad8c5a62dd8110da5e418
sayed atef <sayedatef6293@gmail.com>
2023-03-16 22:41:24 +0000

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main function
-*return always (0)
+* main - entry point
+* Return: always (0) (success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/*return void*/
+	/* return void */
 	return (0);
 }
