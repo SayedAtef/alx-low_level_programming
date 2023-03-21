49fb5f80d9aac027aca11370c3909af6374cf544
sayed atef <sayedatef6293@gmail.com>
2023-03-21 20:19:15 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - prints alphabet in lowercase
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	int j;
+	int i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
