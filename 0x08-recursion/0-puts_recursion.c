46bda0937b630352595feca08136a33e8cd5d9ad
sayed atef <sayedatef6293@gmail.com>
2023-04-05 23:31:12 +0000

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
-  * _puts_recursion - represents puts function with recursion
-  * @s: input to be printed
-  * Return: always (0)
-  */
-
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
