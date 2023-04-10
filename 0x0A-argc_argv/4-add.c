6cf32da480902e2a80db883fd5908d923ecb53f8
sayed atef <sayedatef6293@gmail.com>
2023-04-10 22:00:57 +0000

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,23 +3,23 @@
 #include <ctype.h>
 #include <string.h>
 /**
- * check_num - check - string there are digit
+ * check_number - check - string there are digit
  * @str: array str
  *
  * Return: Always 0 (Success)
  */
-int check_num(char *str)
+int check_number(char *str)
 {
-	unsigned int count;
+	unsigned int counter;
 
-	count = 0;
+	counter = 0;
 	while (count < strlen(str))
 	{
-		if (!isdigit(str[count]))
+		if (!isdigit(str[counter]))
 		{
 			return (0);
 		}
-		count++;
+		counter++;
 	}
 	return (1);
 }
@@ -34,24 +34,24 @@ int check_num(char *str)
 
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
+	int counter;
+	int string_to_integer;
 	int sum = 0;
 
-	count = 1;
-	while (count < argc)
+	counter = 1;
+	while (counter < argc)
 	{
-		if (check_num(argv[count]))
+		if (check_number(argv[counter]))
 		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
+			string_to_integer = atoi(argv[counter]);
+			sum += string_to_integer;
 		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		count++;
+		counter++;
 	}
 
 	printf("%d\n", sum);
