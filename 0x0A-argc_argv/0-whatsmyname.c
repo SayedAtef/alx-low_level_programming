3fa90ecee29edccf80653f1212f361b4b2572875
sayed atef <sayedatef6293@gmail.com>
2023-04-10 21:16:52 +0000

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
   * @argv: array of those arguments
   * Return: Always (0)
   */
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+
 	return (0);
 }
