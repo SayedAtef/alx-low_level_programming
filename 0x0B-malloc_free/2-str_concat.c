13f39ac25493c1c670206ca248e2e120cdeb5d47
sayed atef <sayedatef6293@gmail.com>
2023-04-11 20:53:03 +0000

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * str_concat - returns pointer to new aloc with two strings concatenated
+  * @s1: first input to be concatenated
+  * @s2: second input to be concatenated
+  *
+  * Return: pointer to concatenation of s1 and s2
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *final;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+
+	final = malloc(sizeof(char) * (i + j + 1));
+
+	if (final == NULL)
+		return (NULL);
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		final[i] = s1[i];
+		i++
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		final[i] = s2[j];
+		i++;
+		j++;
+	}
+	final[i] = '\0';
+
+	return (final);
+}
