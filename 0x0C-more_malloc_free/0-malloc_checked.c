354592c6a5f63b708640d41e3612aead71305d75
sayed atef <sayedatef6293@gmail.com>
2023-04-13 18:49:14 +0000

    add readme file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * malloc_checked - allocates memory with length argument
+  * @b: length to be allocated
+  *
+  * Return: (pointer to allocated memory) | (98) Error
+  */
+void *malloc_checked(unsigned int b)
+{
+	char *final;
+
+	final = malloc(b);
+	if (b == NULL)
+		return (98);
+	return final;
+}
