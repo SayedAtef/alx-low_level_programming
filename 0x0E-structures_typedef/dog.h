b580d46f7713f097d3c8c7b3e18fe3876cb33daf
sayed atef <sayedatef6293@gmail.com>
2023-04-17 19:08:12 +0000

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - contains basic info about dog
+  * @name: the dog's name
+  * @age: the dog's age
+  * @owner: the dog's owner
+  *
+  * Description: this a basic dog struct
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
