1b31765828ba8ba566817513027fe01992a0a706
sayed atef <sayedatef6293@gmail.com>
2023-04-20 22:50:18 +0000

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	var_start(stringList, n);
 
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(stringList, char *);
 
