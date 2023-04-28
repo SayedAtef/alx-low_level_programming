173594700282df157e865437f429c906cdcc037b
sayed atef <sayedatef6293@gmail.com>
2023-04-28 01:25:32 +0000

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,15 @@
 section .data
-	message db 'Hello, Holberton', 10, 0
+	message: db 'Hello, Holberton', 10, 0
 
 section.text
 	global main
 	extern printf
 
 main:
-	push rbp
-	mov rbp, rsb
-	sub rsb, 16
-
-	lea rdi, [message]
-	mov al, 0
+	push message
 	call printf
 
-	add rsp, 16
-	mov rsp, rhp
-	pop rbp
+	add rsp, 8
+        :x
+
 	ret
