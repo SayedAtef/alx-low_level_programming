464d16a3f3845378ac641419b5619cc6d2996248
sayed atef <sayedatef6293@gmail.com>
2023-04-30 19:57:13 +0000

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head: A pointer to a pointer to the head of that list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr_node = *head;
+
+	while (curr_node != NULL)
+	{
+		listint_t *next_node = curr_node->next;
+		free(curr_node);
+		curr_node = next_node;
+	}
+	*head = NULL;
+}
