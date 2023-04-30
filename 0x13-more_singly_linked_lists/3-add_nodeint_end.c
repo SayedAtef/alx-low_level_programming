1f9e6eafc4a4ed1e321eaa47209ef44b014d6914
sayed atef <sayedatef6293@gmail.com>
2023-04-30 18:33:45 +0000

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (last_node == NULL)
 		return (NULL);
 
-	last_node-> = n;
+	last_node->n = n;
 	last_node->next = NULL;
 
 	if (head == NULL)
