bfad26036ecbf085174feee9bf38600bd3041932
sayed atef <sayedatef6293@gmail.com>
2023-05-01 20:25:53 +0000

    trail for 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (count);
-
 	while (h != NULL)
 	{
 		printf("%d\n", (h->n));
-		count++;
 		h = h->next;
+		count++;
 	}
 
 	return (count);
