2ec170cf27bad48d3400d7e868b632547f6a1079
sayed atef <sayedatef6293@gmail.com>
2023-05-01 20:36:57 +0000

    trail for 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
  * print_listint - prints all elements of listint_t list
  * @h: A pointer to the head of the list
  *
- * Return: The number of nodes
+ * Return: The size of list
  */
 size_t print_listint(const listint_t *h)
 {
