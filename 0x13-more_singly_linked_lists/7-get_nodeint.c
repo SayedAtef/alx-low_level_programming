18e9a50e9ab05930541cd28274453939dacda2fe
sayed atef <sayedatef6293@gmail.com>
2023-05-01 18:46:07 +0000

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
 
-	while (head != NULL)
+	while (head != NULL && counter < index)
 	{
 		if (counter == index)
 			return (head);
