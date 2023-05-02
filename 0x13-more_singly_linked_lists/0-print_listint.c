84602d151ba2275d973bb754f2f3b43baf36e79b
sayed atef <sayedatef6293@gmail.com>
2023-05-02 12:09:38 +0000

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,19 @@
 /**
  * print_listint - Prints all the elements of a listint_t linked list
  * @h: Pointer to the head node of the linked list
+ *
  * Return: Number of nodes in the linked list
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+	size_t list_size = 0;
 
-    while (h != NULL) {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		list_size++;
+	}
 
-    return (count);
+	return (list_size);
 }
