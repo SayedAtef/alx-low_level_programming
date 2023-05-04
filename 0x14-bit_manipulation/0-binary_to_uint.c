840340bc3cf445e6c308b2162a3315723c89c153
sayed atef <sayedatef6293@gmail.com>
2023-05-04 20:02:06 +0000

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts binary to decimal
+ * @b: pointer to that binary
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	if (!b)
+		return (0);
+
+	size_t length = strlen(b);
+	unsigned int base_num = 1, decimal = 0;
+	int i;
+
+	for (i = length; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[1] != '1')
+			return (0);
+
+		if (b[i] != '0')
+			decimal += base;
+
+		base *= 2;
+	}
+
+	return (decimal);
+}
