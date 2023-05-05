4e9c4147cd3bd5eda90fadfc1b8e1dfeea42ed39
sayed atef <sayedatef6293@gmail.com>
2023-05-05 00:09:18 +0000

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
   *
   * Return: 0 or -1
   */
-int get_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
