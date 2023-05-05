a470702c206eb40cb0ec451d44cb0a340982f0cf
sayed atef <sayedatef6293@gmail.com>
2023-05-05 00:02:26 +0000

    3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,5 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	return ((*n >> index) | index);
+	*n |= 1 << index;
+
+	return (1);
 }
