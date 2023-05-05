ef1b99534ab018448d7d79f908bf0efc129eb6e2
sayed atef <sayedatef6293@gmail.com>
2023-05-05 00:23:18 +0000

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  * clear_bit - clears the value of a bit 
-  * @n: given number
-  * @index: index of bit to be cleared
-  *
-  * Return: 1 for success or -1 for faliure
-  */
+ * clear_bit - clears the value of a bit
+ * @n: given number
+ * @index: index of bit to be cleared
+ *
+ * Return: 1 for success or -1 for faliure
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
