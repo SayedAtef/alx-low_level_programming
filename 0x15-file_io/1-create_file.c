9f620ee3909152b0a9012eb35dce71853583070d
sayed atef <sayedatef6293@gmail.com>
2023-05-12 11:54:03 +0000

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (length == NULL)
+	if (length == 0)
 		bytes_stored = write(fd, text_content, length);
 
 	close(fd);
