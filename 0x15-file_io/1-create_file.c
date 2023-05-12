da2a50bde218181af353b8d6d666871e9a44c574
sayed atef <sayedatef6293@gmail.com>
2023-05-12 11:50:53 +0000

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: A pointer to the file to be created
+ * @text_content: A pointer to the content of that file
+ *
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t length = strlen(text_content), bytes_stored = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	
+	if (fd == -1)
+		return (-1);
+
+	if (length == NULL)
+		bytes_stored = write(fd, text_content, length);
+
+	close(fd);
+
+	return (bytes_stored != length ? -1 : 1);
+}
