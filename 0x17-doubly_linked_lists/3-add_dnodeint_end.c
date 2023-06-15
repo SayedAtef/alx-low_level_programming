8ff269d2195478766341c437f2949b651a97b526
sayed atef <sayedatef6293@gmail.com>
2023-06-15 18:39:59 +0000

    3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,30 +7,30 @@
  *
  * Return: address of the new element, or NULL if failed
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *temp;
+	dlistint_t *new_node;
 
-	temp = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 
-	if (temp == NULL)
+	if (new_node == NULL)
 		return (NULL);
-	temp->n = n;
-	temp->next = NULL;
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
-		temp->prev = NULL;
-		*head = temp;
-		return (temp);
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
 	}
 
 	while ((*head)->next != NULL)
 		*head = (*head)->next;
 
-	(*head)->next = temp;
-	temp->prev = *head;
+	(*head)->next = new_node;
+	new_node->prev = *head;
 
-	return (temp);
+	return (new_node);
 }
