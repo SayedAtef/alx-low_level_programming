eaa9ae6c00de56b43ebcf0ea1b66d276fb061b8b
sayed atef <sayedatef6293@gmail.com>
2023-06-15 18:54:38 +0000

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - free a dlistint_t list from memory
+ * @head: pointer to head of the linked list
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->next_node;
+		free(head);
+		head = next_node;
+	}
+}
