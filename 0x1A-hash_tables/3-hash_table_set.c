cc9b82993dba2fcf9764a715a070f36dd95e78d4
sayed atef <sayedatef6293@gmail.com>
2023-07-12 19:04:44 +0000

    3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: if failure - 0 or Otherwise - 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *n;
+	char *vc;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	vc = strdup(value);
+	if (vc == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = vc;
+			return (1);
+		}
+	}
+
+	n = malloc(sizeof(hash_node_t));
+	if (n == NULL)
+	{
+		free(vc);
+		return (0);
+	}
+	n->key = strdup(key);
+	if (n->key == NULL)
+	{
+		free(n);
+		return (0);
+	}
+	n->value = vc;
+	n->next = ht->array[index];
+	ht->array[index] = n;
+
+	return (1);
+}
